bedc38e9c2d4e62b4955a7eca8e0c142579bf4ed
dain <57316771+DainPark-web@users.noreply.github.com>
2022-12-13 01:30:56 +0100

    update

--- src/filter.ts
+++ src/filter.ts
@@ -1,79 +1,95 @@
-interface calI{
-    [key: string]: number;
-   
+interface calI {
+  [key: string]: number;
 }
 
 export class textFilter {
-    private getText:string
-    private words:any[]
-    private letters:any[]
+  private readonly getText: string;
+  private words: any[];
+  private letters: any[];
+  private sentences: any[];
 
-    constructor(text:string){
-        this.getText = text;
-        this.words = [];
-        this.letters = [];
-    }
-    private cal(list:string[]):calI{
-        const newList = list.map((t) => t.toLowerCase());
-        const calO:any = {};
+  constructor(text: string) {
+    this.getText = text;
+    this.words = [];
+    this.letters = [];
+    this.sentences = [];
+  }
+  private cal(list: string[]): calI {
+    const newList = list.map((t) => t.toLowerCase());
+    const calO: any = {};
 
-        newList.forEach((v) => {
-            if(calO[v]){
-                calO[v] = calO[v] + 1;
-            }else{
-                calO[v] = 1;
-            }
-        })
+    newList.forEach((v) => {
+      if (calO[v]) {
+        calO[v] = calO[v] + 1;
+      } else {
+        calO[v] = 1;
+      }
+    });
 
-        return calO
-    }
+    return calO;
+  }
 
-    private getUnique(list:string[]):string[]{
-        const unique = list.map((letter) => {
-            return letter.toUpperCase();
-        })
-        const uniqueSet = new Set(unique);
-        const uniqueList = [...uniqueSet];
+  private getUnique(list: string[]): string[] {
+    const unique = list.map((letter) => {
+      return letter.toUpperCase();
+    });
+    const uniqueSet = new Set(unique);
+    const uniqueList = [...uniqueSet];
 
-        return uniqueList
-    }
+    return uniqueList;
+  }
 
+  getLetters() {
+    this.letters = this.getText.split("").filter((letter) => {
+      if (letter !== " ") {
+        return letter;
+      }
+    });
+    const uniqueList = this.getUnique(this.letters);
+    return {
+      letters: this.letters,
+      unique: uniqueList,
+      length: this.letters.length,
+      cal: this.cal(this.letters),
+    };
+  }
 
-    getLetters(){
-        this.letters = this.getText.split("").filter((letter) => {
-            if(letter !== " "){
-                return letter;
-            }
-        });
-        const uniqueList = this.getUnique(this.letters);
-        return {
-            letters: this.letters,
-            unique: uniqueList,
-            length: this.letters.length,
-            cal: this.cal(this.letters)
-        }
-    }
+  getWords() {
+    this.words = this.getText.split(" ");
+    const uniqueList = this.getUnique(this.words);
+    return {
+      words: this.words,
+      unique: uniqueList,
+      length: this.words.length,
+      cal: this.cal(this.words),
+    };
+  }
 
-    getWords(){
-        this.words = this.getText.split(" ")
-        const uniqueList = this.getUnique(this.words);
-        return {
-            words: this.words,
-            unique: uniqueList,
-            length: this.words.length,
-            cal: this.cal(this.words),
-            
-        }
-    }
+  getSentences() {
+    const list = this.getText.split(".").map((text) => {
+      const newT = text.trim();
+      return newT;
+    });
+    this.sentences = list.filter((sen) => sen !== "");
 
+    const uniqueList = this.getUnique(this.sentences);
 
-    getData(){
-        const letter = this.getLetters();
-        const word = this.getWords();
-        return [
-            letter,
-            word
-        ]
-    }
+    return {
+      sentences: this.sentences,
+      unique: uniqueList,
+      length: this.sentences.length,
+      cal: this.cal(this.sentences),
+    };
+  }
+
+  getData() {
+    const letter = this.getLetters();
+    const word = this.getWords();
+    const sentence = this.getSentences();
+    return {
+        letter,
+        word,
+        sentence
+    };
+  }
 }
-
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { textFilter } from "./filter";
 
-const d = new textFilter("ddd ddd fff hhh");
-// console.log(d.getData());
-console.log(d.getWords());
+const d = new textFilter(`dain park. 
+hi your name is dain park. dain park`);
+console.log(d.getData());
+// console.log(d.getWords());
+// console.log(d.getSentences());
 // console.log(d.getLetters());
 
 
