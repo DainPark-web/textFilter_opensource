26e0a75b605a27affc10f524ed778ff3ad05600e
dain <57316771+DainPark-web@users.noreply.github.com>
2022-12-13 13:47:03 +0100

    updae

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ const d = new textFilter(`dain park.
 hi your name is dain park. Dain park. Hi good morning. Dain is designer`);
 // console.log(d.getData());
 // console.log(d.getWords());
-console.log(d.getSentences());
-// console.log(d.getLetters());
+// console.log(d.getSentences());
+console.log(d.getLetters());
 
 
 export { textFilter };
